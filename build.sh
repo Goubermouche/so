71a7095cc65207e2e31243dcad8b42c7faf7e2ca
nella <simontupy64@gmail.com>
2026-05-04 13:54:38 +0200

    Parallelize build.

--- build.sh
+++ build.sh
@@ -1,15 +1,29 @@
 #!/bin/sh
 set -e
-mkdir -p out
+
+mkdir -p out/obj 
 export LD_LIBRARY_PATH=/run/opengl-driver/lib:$LD_LIBRARY_PATH
+
 srcs_cu=$(find src -name '*.cu' 2>/dev/null)
 srcs_cc=$(find src -name '*.cc' 2>/dev/null)
-nvcc_flags="-I src -O2 -Wno-deprecated-gpu-targets -arch=sm_61"
+export nvcc_flags="-I src -O2 -Wno-deprecated-gpu-targets -arch=sm_61"
 nvcc_libs="-lz3"
-output="out/so"
-if command -v bear >/dev/null 2>&1; then
-	bear -- nvcc $nvcc_flags -o $output $srcs_cu $srcs_cc $nvcc_libs
-else
-	nvcc $nvcc_flags -o $output $srcs_cu $srcs_cc $nvcc_libs
-fi
-echo "built: $output"
+output="out/sup"
+
+# job count
+cores=$(nproc)
+jobs=$(( cores > 2 ? cores - 2 : 1 ))
+
+# build in parallel
+echo "$srcs_cu $srcs_cc" | tr ' ' '\n' | xargs -P $jobs -I {} sh -c '
+	src="{}"
+	obj="out/obj/$(basename "$src").o"
+	echo $src
+	nvcc $nvcc_flags -c "$src" -o "$obj"
+'
+
+objects=$(find out/obj -name '*.o')
+
+# link
+echo "linking $output"
+nvcc $nvcc_flags $objects $nvcc_libs -o "$output"
